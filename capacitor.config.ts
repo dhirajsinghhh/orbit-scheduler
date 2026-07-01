8e41bf596ff9e52fd12f7b9567839486240a3ef0
dhirajsinghhh <154895692+dhirajsinghhh@users.noreply.github.com>
2026-07-01 20:26:44 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dhiraj.orbitscheduler',
+  appName: 'Orbit Scheduler',
+  webDir: 'dist'
+};
+
+export default config;
